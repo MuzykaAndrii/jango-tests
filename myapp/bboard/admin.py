2946e863c337f0f564c14ffe7a5744c00ffd0ea1
andrymyzik <andrymyzik@gmail.com>
2023-01-20 01:00:43 +0200

    added theme models, bound they with products and configurated admin panel for themes

--- myapp/bboard/admin.py
+++ myapp/bboard/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 
 from .models import Bb
+from .models import Theme
+
 
 class BbAdmin(admin.ModelAdmin):
-    list_display = ('title', 'content', 'price', 'published')
+    list_display = ('title', 'content', 'price', 'published', 'theme')
     list_display_links = ('title', 'content')
     search_fields = ('title', 'content')
 
 admin.site.register(Bb, BbAdmin)
+admin.site.register(Theme)
--- myapp/bboard/models.py
+++ myapp/bboard/models.py
@@ -6,8 +6,21 @@ class Bb(models.Model):
     content = models.TextField(null=True, blank=True, verbose_name='Some description')
     price = models.FloatField(null=True, blank=True, verbose_name='Price')
     published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Date of published')
+    theme = models.ForeignKey('Theme', null=True, on_delete=models.PROTECT, verbose_name='Theme')
 
     class Meta:
         verbose_name_plural = 'Goods'
         verbose_name = 'Good'
         ordering = ['-published']
+
+
+class Theme(models.Model):
+    name = models.CharField(max_length=20, db_index=True, verbose_name='Theme name')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Themes'
+        verbose_name = 'Theme'
+        ordering = ('name',)
