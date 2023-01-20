7a73f129695619f38a3321b33e4d3542216b616a
andrymyzik <andrymyzik@gmail.com>
2023-01-20 02:40:46 +0200

    added pages by theme and add page

--- myapp/bboard/urls.py
+++ myapp/bboard/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import index
+from .views import index, by_theme, BbCreateView
 
 
 urlpatterns = [
-    path('', index)
+    path('', index, name='index'),
+    path('<int:theme_id>', by_theme, name='by_theme'),
+    path('add/', BbCreateView.as_view(), name='add'),
 ]
--- myapp/bboard/views.py
+++ myapp/bboard/views.py
@@ -1,7 +1,32 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
-from .models import Bb
+from .models import Bb, Theme
+from .forms import BbForm
+
+
+class BbCreateView(CreateView):
+    template_name = 'bboard/create.html'
+    form_class = BbForm
+    success_url = reverse_lazy('index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['themes'] = Theme.objects.all()
+        return context
+
 
 def index(request):
     all_goods = Bb.objects.all()
     return render(request, 'bboard/index.html', {'bbs': all_goods})
+
+
+def by_theme(request, theme_id):
+    """"returns all rubrics by certain theme"""
+    all_goods = Bb.objects.filter(theme=theme_id)
+    themes = Theme.objects.all()
+    current_theme = Theme.objects.get(pk=theme_id)
+
+    context = {'bbs': all_goods, 'current_theme': current_theme, 'themes': themes}
+    return render(request, 'bboard/by_rubric.html', context)
